0a45f4dab5f28f9586576bd56be8c8461b9cf97a
Shivam Bonde <156459718+ShivamBonde@users.noreply.github.com>
2025-05-07 19:58:00 +0530

    Update 7_AND_XOR.py

--- 7_AND_XOR.py
+++ 7_AND_XOR.py
@@ -1,15 +1,15 @@
-# Input string
-input_str = "Hello World"
+string="hello world"
+a=len(string)
 
-# Convert each character to its ASCII value, perform bitwise AND and XOR with 127, and display the result
-and_result = ""
-xor_result = ""
-
-for char in input_str:
-    ascii_value = ord(char)  # Get ASCII value of the character
-    and_result += chr(ascii_value & 127)  # AND with 127 and convert back to character
-    xor_result += chr(ascii_value ^ 127)  # XOR with 127 and convert back to character
-
-print("Original String: ", input_str)
-print("AND with 127:    ", and_result)
-print("XOR with 127:    ", xor_result)
+print("and: ")
+for i in range(a):
+    ans1=ord(string[i]) & 127
+    print(ans1,end=" ")
+    
+print("\nxor: ")
+for i in range(a):
+    ans2=ord(string[i]) ^ 127
+    print(ans2,end=" ")
+    
+    
+    
